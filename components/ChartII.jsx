4c2e4e9f277493400f8a9a6e5d3303373c62d6a6
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-10-22 12:25:53 +0300

    finish working on cartII layout

--- components/ChartII.jsx
+++ components/ChartII.jsx
@@ -14,50 +14,17 @@ import {
   PopoverBody,
 } from "@chakra-ui/react";
 import { FaTrash } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const CartPage = () => {
   const data = JSON.parse(localStorage.getItem("state"));
   const cartData = data?.ChartData;
-  console.log("cartData ", cartData);
   const ml = useBreakpointValue({ base: "column", md: "row" });
-  console.log("ml ", ml);
-  const cartItems = [
-    {
-      id: 1,
-      name: "Product 1",
-      image: "https://via.placeholder.com/150",
-      previewImage: "https://via.placeholder.com/300",
-      price: 10.99,
-      quantity: 2,
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      image: "https://via.placeholder.com/150",
-      previewImage: "https://via.placeholder.com/300",
-      price: 5.99,
-      quantity: 1,
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      image: "https://via.placeholder.com/150",
-      previewImage: "https://via.placeholder.com/300",
-      price: 5.99,
-      quantity: 1,
-    },
-    {
-      id: 4,
-      name: "Product 3",
-      image: "https://via.placeholder.com/150",
-      previewImage: "https://via.placeholder.com/300",
-      price: 5.99,
-      quantity: 1,
-    },
-  ];
-
   const [previewImage, setPreviewImage] = useState("");
+
+  useEffect(() => {
+    window.scrollTo(0, -200);
+  }, []);
 
   const handleRemoveItem = (id) => {
     // remove item from cart
@@ -75,10 +42,11 @@ const CartPage = () => {
       <Flex direction={{ base: "column", md: "row" }}>
         <Box flex="1">
           {cartData.map((item) => {
+            const { id, title, image, category, price, quantity } = item;
             if (ml === "row") {
               return (
                 <Flex
-                  key={item.id}
+                  key={id}
                   bg="white"
                   p={4}
                   rounded="md"
@@ -98,94 +66,92 @@ const CartPage = () => {
                   onClick={() => handlePreviewImage(item)}
                 >
                   <Box position="relative">
-                    <Image src={item.image} alt={item.title} boxSize={{ base: "50px", md: "75px" }} mr={4} />
+                    <Image src={image} alt={title} boxSize={{ base: "50px", md: "75px" }} mr={4} />
                   </Box>
                   <Box>
-                    <Text fontWeight="bold">{item.title}</Text>
+                    <Text fontWeight="bold">{title}</Text>
 
                     <Text fontSize="sm" color="gray.500" fontStyle="italic">
-                      {item.category}
+                      {category}
                     </Text>
                     <Text fontSize="sm" color="gray.500">
-                      ${item.price} x {item.quantity}
+                      ${price} x {quantity}
                     </Text>
                   </Box>
                   <IconButton
                     icon={<FaTrash />}
                     aria-label="Remove item"
                     ml="auto"
-                    onClick={() => handleRemoveItem(item.id)}
+                    onClick={() => handleRemoveItem(id)}
                   />
                 </Flex>
+              );
+            }
+
+            if (ml === "column") {
+              return (
+                <Popover
+                  key={id}
+                  // returnFocusOnClose={false}
+                  // isOpen={isOpen}
+                  // onClose={onClose}
+                  strategy="fixed"
+                  placement="bottom"
+                  closeOnBlur={true}
+                >
+                  <PopoverTrigger>
+                    <Flex
+                      bg="white"
+                      p={4}
+                      rounded="md"
+                      shadow="md"
+                      alignItems="center"
+                      mb={4}
+                      _hover={{
+                        bg: "gray.400",
+                        cursor: "pointer",
+                        transform: "scale(1.01)",
+                        transition: "all 0.2s ease-in-out",
+                      }}
+                      _active={{
+                        transform: "scale(0.99)",
+                      }}
+                      transition="all 0.2s ease-in-out"
+                      onClick={() => handlePreviewImage(item)}
+                    >
+                      <Box position="relative">
+                        <Image src={image} alt={title} boxSize={{ base: "50px", md: "75px" }} mr={4} />
+                      </Box>
+                      <Box>
+                        <Text fontWeight="bold">{title}</Text>
+                        <Text fontSize="sm" color="gray.500" fontStyle="italic">
+                          {category}
+                        </Text>
+                        <Text fontSize="sm" color="gray.500">
+                          ${price} x {quantity}
+                        </Text>
+                      </Box>
+                      <IconButton
+                        icon={<FaTrash />}
+                        aria-label="Remove item"
+                        ml="auto"
+                        onClick={() => handleRemoveItem(id)}
+                      />
+                    </Flex>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverBody>
+                      <Image src={image} alt={title} boxSize={{ base: "300px" }} mr={4} />
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
               );
             }
           })}
         </Box>
 
-        {ml === "column" && (
-          <Box flex="1">
-            {cartData.map((item) => (
-              <Popover
-                key={item.id}
-                // returnFocusOnClose={false}
-                // isOpen={isOpen}
-                // onClose={onClose}
-                strategy="fixed"
-                placement="bottom"
-                closeOnBlur={true}
-              >
-                <PopoverTrigger>
-                  <Flex
-                    bg="white"
-                    p={4}
-                    rounded="md"
-                    shadow="md"
-                    alignItems="center"
-                    mb={4}
-                    _hover={{
-                      bg: "gray.400",
-                      cursor: "pointer",
-                      transform: "scale(1.01)",
-                      transition: "all 0.2s ease-in-out",
-                    }}
-                    _active={{
-                      transform: "scale(0.99)",
-                    }}
-                    transition="all 0.2s ease-in-out"
-                    onClick={() => handlePreviewImage(item)}
-                  >
-                    <Box position="relative">
-                      <Image src={item.image} alt={item.title} boxSize={{ base: "50px", md: "75px" }} mr={4} />
-                    </Box>
-                    <Box>
-                      <Text fontWeight="bold">{item.title}</Text>
-                      <Text fontSize="sm" color="gray.500" fontStyle="italic">
-                        {item.category}
-                      </Text>
-                      <Text fontSize="sm" color="gray.500">
-                        ${item.price} x {item.quantity}
-                      </Text>
-                    </Box>
-                    <IconButton
-                      icon={<FaTrash />}
-                      aria-label="Remove item"
-                      ml="auto"
-                      onClick={() => handleRemoveItem(item.id)}
-                    />
-                  </Flex>
-                </PopoverTrigger>
-                <PopoverContent>
-                  <PopoverArrow />
-                  <PopoverCloseButton />
-                  <PopoverBody>
-                    {" "}
-                    <Image src={item.image} alt={item.title} boxSize={{ base: "300px" }} mr={4} />
-                  </PopoverBody>
-                </PopoverContent>
-              </Popover>
-            ))}
-          </Box>
-        )}
         {ml === "row" && (
           <Box
             display="flex"
