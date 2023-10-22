ddead4fcf5ca08594481b5b51bdcb16cd98cc71e
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-10-22 11:35:47 +0300

    working on cart page phase I

--- components/ChartII.jsx
+++ components/ChartII.jsx
@@ -1,9 +1,25 @@
-import { Box, Flex, Heading, IconButton, Image, Text, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  Image,
+  Text,
+  useBreakpointValue,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverBody,
+} from "@chakra-ui/react";
 import { FaTrash } from "react-icons/fa";
-
 import { useState } from "react";
 
 const CartPage = () => {
+  const data = JSON.parse(localStorage.getItem("state"));
+  const cartData = data?.ChartData;
+  console.log("cartData ", cartData);
   const ml = useBreakpointValue({ base: "column", md: "row" });
   console.log("ml ", ml);
   const cartItems = [
@@ -18,6 +34,22 @@ const CartPage = () => {
     {
       id: 2,
       name: "Product 2",
+      image: "https://via.placeholder.com/150",
+      previewImage: "https://via.placeholder.com/300",
+      price: 5.99,
+      quantity: 1,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      image: "https://via.placeholder.com/150",
+      previewImage: "https://via.placeholder.com/300",
+      price: 5.99,
+      quantity: 1,
+    },
+    {
+      id: 4,
+      name: "Product 3",
       image: "https://via.placeholder.com/150",
       previewImage: "https://via.placeholder.com/300",
       price: 5.99,
@@ -42,45 +74,118 @@ const CartPage = () => {
       </Heading>
       <Flex direction={{ base: "column", md: "row" }}>
         <Box flex="1">
-          {cartItems.map((item) => (
-            <Flex
-              key={item.id}
-              bg="white"
-              p={4}
-              rounded="md"
-              shadow="md"
-              alignItems="center"
-              mb={4}
-              _hover={{
-                bg: "gray.400",
-                cursor: "pointer",
-                transform: "scale(1.01)",
-                transition: "all 0.2s ease-in-out",
-              }}
-              _active={{
-                transform: "scale(0.99)",
-              }}
-              transition="all 0.2s ease-in-out"
-              onClick={() => handlePreviewImage(item)}
-            >
-              <Box position="relative">
-                <Image src={item.image} alt={item.name} boxSize={{ base: "50px", md: "75px" }} mr={4} />
-              </Box>
-              <Box>
-                <Text fontWeight="bold">{item.name}</Text>
-                <Text fontSize="sm" color="gray.500">
-                  ${item.price} x {item.quantity}
-                </Text>
-              </Box>
-              <IconButton
-                icon={<FaTrash />}
-                aria-label="Remove item"
-                ml="auto"
-                onClick={() => handleRemoveItem(item.id)}
-              />
-            </Flex>
-          ))}
+          {cartData.map((item) => {
+            if (ml === "row") {
+              return (
+                <Flex
+                  key={item.id}
+                  bg="white"
+                  p={4}
+                  rounded="md"
+                  shadow="md"
+                  alignItems="center"
+                  mb={4}
+                  _hover={{
+                    bg: "gray.400",
+                    cursor: "pointer",
+                    transform: "scale(1.01)",
+                    transition: "all 0.2s ease-in-out",
+                  }}
+                  _active={{
+                    transform: "scale(0.99)",
+                  }}
+                  transition="all 0.2s ease-in-out"
+                  onClick={() => handlePreviewImage(item)}
+                >
+                  <Box position="relative">
+                    <Image src={item.image} alt={item.title} boxSize={{ base: "50px", md: "75px" }} mr={4} />
+                  </Box>
+                  <Box>
+                    <Text fontWeight="bold">{item.title}</Text>
+
+                    <Text fontSize="sm" color="gray.500" fontStyle="italic">
+                      {item.category}
+                    </Text>
+                    <Text fontSize="sm" color="gray.500">
+                      ${item.price} x {item.quantity}
+                    </Text>
+                  </Box>
+                  <IconButton
+                    icon={<FaTrash />}
+                    aria-label="Remove item"
+                    ml="auto"
+                    onClick={() => handleRemoveItem(item.id)}
+                  />
+                </Flex>
+              );
+            }
+          })}
         </Box>
+
+        {ml === "column" && (
+          <Box flex="1">
+            {cartData.map((item) => (
+              <Popover
+                key={item.id}
+                // returnFocusOnClose={false}
+                // isOpen={isOpen}
+                // onClose={onClose}
+                strategy="fixed"
+                placement="bottom"
+                closeOnBlur={true}
+              >
+                <PopoverTrigger>
+                  <Flex
+                    bg="white"
+                    p={4}
+                    rounded="md"
+                    shadow="md"
+                    alignItems="center"
+                    mb={4}
+                    _hover={{
+                      bg: "gray.400",
+                      cursor: "pointer",
+                      transform: "scale(1.01)",
+                      transition: "all 0.2s ease-in-out",
+                    }}
+                    _active={{
+                      transform: "scale(0.99)",
+                    }}
+                    transition="all 0.2s ease-in-out"
+                    onClick={() => handlePreviewImage(item)}
+                  >
+                    <Box position="relative">
+                      <Image src={item.image} alt={item.title} boxSize={{ base: "50px", md: "75px" }} mr={4} />
+                    </Box>
+                    <Box>
+                      <Text fontWeight="bold">{item.title}</Text>
+                      <Text fontSize="sm" color="gray.500" fontStyle="italic">
+                        {item.category}
+                      </Text>
+                      <Text fontSize="sm" color="gray.500">
+                        ${item.price} x {item.quantity}
+                      </Text>
+                    </Box>
+                    <IconButton
+                      icon={<FaTrash />}
+                      aria-label="Remove item"
+                      ml="auto"
+                      onClick={() => handleRemoveItem(item.id)}
+                    />
+                  </Flex>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverBody>
+                    {" "}
+                    <Image src={item.image} alt={item.title} boxSize={{ base: "300px" }} mr={4} />
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            ))}
+          </Box>
+        )}
         {ml === "row" && (
           <Box
             display="flex"
