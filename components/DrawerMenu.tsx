b7553a9cbd1acfef735e7606bb770333bc7a7c79
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-11-25 00:40:49 +0300

    separate drawer menu

---
+++ components/DrawerMenu.tsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useNavigate } from "react-router-dom";
+
+interface Types {
+  props: {
+    userToken: string;
+    handleLogOut: () => void;
+  };
+}
+
+const DrawerMenu: React.FC<Types["props"]> = ({ userToken, handleLogOut }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+  const navigate = useNavigate();
+  const [categoryClicked, setCategoryClicked] = useState({ category: "", clicked: false });
+
+  useEffect(() => {
+    const pageDocument = document.location.pathname.split("/")[1];
+    setCategoryClicked({ category: pageDocument, clicked: true });
+    console.log("pageDocument from drawer", pageDocument);
+  }, [document.location.pathname]);
+
+  const handleNavigation = (e) => {
+    //get the name of the clicked element
+    console.log("e.target.id", e.target.id);
+    const category = e.target.id;
+    navigate(`/${category}`);
+    setCategoryClicked((prevState) => {
+      if (prevState.category === category) {
+        return { category: "", clicked: false };
+      } else {
+        return { category: category, clicked: true };
+      }
+    });
+
+    onClose();
+  };
+  return (
+    <>
+      <Button
+        ref={btnRef}
+        bg="#c6ebbe"
+        onClick={onOpen}
+        className="hamburger"
+        //use the hamburger icon from chakra-ui
+      >
+        <HamburgerIcon />
+      </Button>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} finalFocusRef={btnRef}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+
+          <DrawerBody>
+            <Box
+              w="90%"
+              className="drawer"
+              //make the background color of the clicked element green
+              bg={categoryClicked.clicked && categoryClicked.category === "home" ? "green" : "white"}
+            >
+              Home
+            </Box>
+
+            <Button
+              w="90%"
+              className="drawer"
+              name="waterSpaces"
+              id="waterSpaces"
+              bg={categoryClicked.clicked && categoryClicked.category === "waterSpaces" ? "#647AA3" : "white"}
+              onClick={(e) => {
+                handleNavigation(e);
+              }}
+            >
+              Water Spaces
+            </Button>
+            <Button
+              w="90%"
+              className="drawer"
+              id="candles"
+              bg={categoryClicked.clicked && categoryClicked.category === "candles" ? "#647AA3" : "white"}
+              onClick={(e) => {
+                handleNavigation(e);
+              }}
+            >
+              Candles
+            </Button>
+            {userToken && (
+              <Button
+                w="90%"
+                className="drawer"
+                id="candles"
+                bg={categoryClicked.clicked && categoryClicked.category === "logout" ? "#647AA3" : "white"}
+                onClick={() => {
+                  handleLogOut();
+                  onClose();
+                }}
+              >
+                logOut
+              </Button>
+            )}
+            {!userToken && (
+              <Button
+                w="90%"
+                className="drawer"
+                id="candles"
+                bg={categoryClicked.clicked && categoryClicked.category === "signin" ? "#647AA3" : "white"}
+                onClick={() => {
+                  navigate("/signin");
+                  onClose();
+                }}
+              >
+                SignIn
+              </Button>
+            )}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default DrawerMenu;
--- components/NavBar_sm_Size.jsx
+++ components/NavBar_sm_Size.jsx
@@ -22,35 +22,12 @@ import { HamburgerIcon } from "@chakra-ui/icons";
 import "../styles/PhoneDrawer.css";
 import "../styles/Header.css";
 import { useEffect } from "react";
+import DrawerMenu from "./DrawerMenu";
 
 export default function NavBar_sm_Size({ cartItemsNumber, userToken, handleLogOut, verifyAdmin }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = useRef();
-  const navigate = useNavigate();
-  const [categoryClicked, setCategoryClicked] = useState({ category: "", clicked: false });
-
   //handle the navigation to the clicked element from the drawer
   //this functionality runs from here not from the action router
-  const handleNavigation = (e) => {
-    //get the name of the clicked element
-    console.log("e.target.id", e.target.id);
-    const category = e.target.id;
-    navigate(`/${category}`);
-    setCategoryClicked((prevState) => {
-      if (prevState.category === category) {
-        return { category: "", clicked: false };
-      } else {
-        return { category: category, clicked: true };
-      }
-    });
 
-    onClose();
-  };
-  useEffect(() => {
-    const pageDocument = document.location.pathname.split("/")[1];
-    setCategoryClicked({ category: pageDocument, clicked: true });
-    console.log("pageDocument from drawer", pageDocument);
-  }, [document.location.pathname]);
   //change the background color of the clicked element
 
   return (
@@ -70,15 +47,7 @@ export default function NavBar_sm_Size({ cartItemsNumber, userToken, handleLogOu
         h={2}
       >
         <Box>
-          <Button
-            ref={btnRef}
-            bg="#c6ebbe"
-            onClick={onOpen}
-            className="hamburger"
-            //use the hamburger icon from chakra-ui
-          >
-            <HamburgerIcon />
-          </Button>
+          <DrawerMenu userToken={userToken} handleLogOut={handleLogOut} />
         </Box>
         <Box>Home</Box>
         <Box
@@ -108,63 +77,6 @@ export default function NavBar_sm_Size({ cartItemsNumber, userToken, handleLogOu
           )}
         </Box>
       </Flex>
-
-      <Drawer isOpen={isOpen} placement="left" onClose={onClose} finalFocusRef={btnRef}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Menu</DrawerHeader>
-
-          <DrawerBody>
-            <Box
-              w="90%"
-              className="drawer"
-              //make the background color of the clicked element green
-              bg={categoryClicked.clicked && categoryClicked.category === "home" ? "green" : "white"}
-            >
-              Home
-            </Box>
-
-            <Button
-              w="90%"
-              className="drawer"
-              name="waterSpaces"
-              id="waterSpaces"
-              bg={categoryClicked.clicked && categoryClicked.category === "waterSpaces" ? "#647AA3" : "white"}
-              onClick={(e) => {
-                handleNavigation(e);
-              }}
-            >
-              Water Spaces
-            </Button>
-            <Button
-              w="90%"
-              className="drawer"
-              id="candles"
-              bg={categoryClicked.clicked && categoryClicked.category === "candles" ? "#647AA3" : "white"}
-              onClick={(e) => {
-                handleNavigation(e);
-              }}
-            >
-              Candles
-            </Button>
-            {userToken && (
-              <Button
-                w="90%"
-                className="drawer"
-                id="candles"
-                bg={categoryClicked.clicked && categoryClicked.category === "logout" ? "#647AA3" : "white"}
-                onClick={() => {
-                  handleLogOut();
-                  onClose();
-                }}
-              >
-                logOut
-              </Button>
-            )}
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
     </>
   );
 }
