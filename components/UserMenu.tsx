785f44c27d7fd6c7f8bf55857430357fe85264be
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-11-26 16:14:03 +0300

    User Menu :  enable login , logout from the menu

--- components/UserMenu.tsx
+++ components/UserMenu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Menu, MenuButton, MenuList, MenuItem, IconButton, Box } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { BiLogOut, BiSolidArchiveOut } from "react-icons/bi";
+import { BiLogOut, BiLogIn, BiSolidArchiveOut } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
 const hoverStyle = {
@@ -10,16 +10,42 @@ const hoverStyle = {
   color: "white",
 };
 
-const UserMenu: React.FC = () => {
+interface ITypes {
+  props: {
+    handleLogOut: () => void;
+    userToken: string;
+  };
+}
+
+const UserMenu: React.FC<ITypes["props"]> = ({ handleLogOut, userToken }) => {
   const navigate = useNavigate();
   return (
     <Box ml="7" mr={"4"}>
       <Menu>
         <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} />
         <MenuList bg={"white"}>
-          <MenuItem bg={"white"} color={"black"} _hover={{ ...hoverStyle }} icon={<BiLogOut size={30} />}>
-            logout
-          </MenuItem>
+          {userToken && (
+            <MenuItem
+              bg={"white"}
+              color={"black"}
+              _hover={{ ...hoverStyle }}
+              icon={<BiLogOut size={30} />}
+              onClick={() => handleLogOut()}
+            >
+              Logout
+            </MenuItem>
+          )}
+          {!userToken && (
+            <MenuItem
+              bg={"white"}
+              color={"black"}
+              _hover={{ ...hoverStyle }}
+              icon={<BiLogIn size={30} />}
+              onClick={() => navigate("/signin")}
+            >
+              Login
+            </MenuItem>
+          )}
           <MenuItem
             bg={"white"}
             color={"black"}
