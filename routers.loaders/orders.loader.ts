5b0eb15180917c01b8b359f06a474e19db1b3bbb
Hasan Baydoun <hasan-net2010@hotmail.com>
2023-11-26 13:54:53 +0300

    convert component to ts

--- routers.loaders/orders.loader.ts
+++ routers.loaders/orders.loader.ts
@@ -1,11 +1,22 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { redirect } from "react-router-dom";
+
+interface ITypes {
+  token: {
+    email: string;
+  };
+}
 
 export default async function OrdersLoader({ params }) {
   let data = {};
   const CARDS_URL = import.meta.env.VITE_BACKEND_URL_CARDS + "/cart/orders";
   const token = localStorage.getItem("token");
-  const { email } = jwtDecode(token);
+  let email: string = "";
+  if (token !== null) {
+    email = jwtDecode<ITypes["token"]>(token).email;
+  }
+
   console.log("loader params", params.waterSpaces);
   // if (params.token === undefined) {
   //   console.log("redirect to signin");
